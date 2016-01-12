ab1bfd28ffbe619c5fa385697aa100a32b3236d2
nathangruber <grubernp@gmail.com>
2016-01-11 18:00:37 -0600

    added w2-d1 hw

---
+++ w2-d1/inheritance.php
@@ -0,0 +1,74 @@
+<?php
+	class ice_skate{
+		var $laces;
+		var $blade;
+		var $rivets;
+		var $boot;
+		var $holders;
+		var $tongue;
+		var $toe;
+		var $outsole;
+		var $heel;
+		var $achillesguard;
+		function __construct(){
+		;
+		}
+		function(support){
+		;
+		}
+		function(glides){
+		;
+		}
+		function(cuts){
+		;
+		}
+		function(protects){
+		;
+		}
+		function(maneuverability){
+		;
+		}
+
+}
+		class bauer extends ice_skate{
+		function __construct(){
+		;
+		}		
+		function(stylish){
+		;
+		}
+		function(lacelock){
+		;
+		}
+		function(tuuk_runner){
+		;
+		}	
+}
+		class ccm extends ice_skate{
+		function __construct(){
+		;
+		}
+		function(custom_insole){
+		;
+		}
+		function(light_weight){
+		;
+		}
+		function(speedblade_holder){
+		;
+		}
+		function(Carbon_Twill_Composite_AttackFrame){
+		;
+		}
+		function(Dual_Zone_Clarino_Liner){
+		;
+		}
+		function(Formula_T6_Pro_Core){
+		;
+		}
+		function(custom_colors){
+		;
+		}
+}
+?>
+
