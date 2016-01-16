64aef07e81eaa849d2a73721fb467914e9613642
nathangruber <grubernp@gmail.com>
2016-01-16 17:32:18 -0600

    made code more readable for Pat
    cleaned it up, the best i could

--- w2-d2/largest.php
+++ w2-d2/largest.php
@@ -1,16 +1,17 @@
 <?php 
-function largest_numbers($array_of_numbers)
-{
+function largest_numbers($array_of_numbers){
    $largest_value = -1;
-   for($x = 0; $x < count($array_of_numbers);$x++){
+   
+	for($x = 0; $x < count($array_of_numbers);$x++){
+	
 	if($largest_value < $array_of_numbers[$x]){
 	($largest_value = $array_of_numbers[$x]);
 	} 
-   }
+    }
 	return($largest_value);
 
 }
-echo  largest_numbers(array( 0,1,2));
+echo largest_numbers(array( 0,1,2));
 
 
 ?>
