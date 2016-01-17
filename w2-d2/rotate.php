f8c2b42a4eaf3423f67822e3eb77bf61c835ccbc
nathangruber <grubernp@gmail.com>
2016-01-16 18:00:29 -0600

    crisped code
    cleaned it up

--- w2-d2/rotate.php
+++ w2-d2/rotate.php
@@ -1,14 +1,17 @@
 <?php
+
 $rotate = array (1,2,3);
-count($rotate);
+	count($rotate);
 $switch = 1;
-for ($i=0; $i < count($rotate) - 1; $i++) {
-	$rotate[$i] = $rotate[$i +1];
+
+	for ($i=0; $i < count($rotate) - 1; $i++){
+		$rotate[$i] = $rotate[$i +1];
 }
 $rotate[$i] = $switch;
 print_r ($rotate);
-for ($i =0; $i <count($rotate); $i++) {
-	$rotate[$i] = $rotate[$i] + 5;
+	
+	for ($i =0; $i <count($rotate); $i++){
+		$rotate[$i] = $rotate[$i] + 5;
 }
 print_r ($rotate);
 ?>
